919a39a156c12a0d711ca995bfade18d2d914805
Mayank <mayank.srmu@gmail.com>
2025-01-12 19:42:06 +0530

    fix: no updates as re-render occurs based on shallow change of the value returned by getSnapshot

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -13,9 +13,6 @@ import { useSyncExternalStore } from "react";
 type ListenerWithSelector<T, U> = [listener: () => void, selectorFunc?: (state: T) => U];
 type StateSetterArgType<T> = ((newState: T) => Partial<T>) | Partial<T> | T;
 
-const ObjectAssign = Object.assign;
-const isArray = Array.isArray;
-
 export const create = <T>(
   storeCreator: (set: (state: StateSetterArgType<T>) => void, get: () => T | null) => T,
 ) => {
@@ -26,15 +23,16 @@ export const create = <T>(
     const oldState = stateRef.k;
     const partial = newState instanceof Function ? newState(stateRef.k!) : newState;
     stateRef.k =
-      partial instanceof Object && !isArray(partial)
-        ? ObjectAssign(stateRef.k!, partial)
+      partial instanceof Object && !Array.isArray(partial)
+        ? { ...stateRef.k!, ...partial }
         : (partial as T);
 
     listeners.forEach(
       ([listener, selectorFunc]) =>
         (!selectorFunc ||
           (oldState &&
-            JSON.stringify(selectorFunc(stateRef.k!)) != JSON.stringify(selectorFunc(oldState)))) &&
+            JSON.stringify(selectorFunc(stateRef.k!)) !==
+              JSON.stringify(selectorFunc(oldState)))) &&
         listener(),
     );
   };
@@ -51,15 +49,10 @@ export const create = <T>(
   const useHook = <U = T>(selectorFunc?: (state: T) => U): U => {
     const getSlice = () => {
       const newValue = selectorFunc!(stateRef.k!);
-      if (!(newValue instanceof Object)) return newValue as U;
-
-      const isArr = isArray(newValue);
-
-      const obj = map.get(selectorFunc!) ?? (isArr ? [] : {});
-      if (isArr) (obj as unknown[]).splice(0, (obj as unknown[]).length, ...newValue);
-      else ObjectAssign(obj, selectorFunc!(stateRef.k!));
-      map.set(selectorFunc!, obj);
-      return obj as U;
+      const obj = map.get(selectorFunc!);
+      const finalValue = JSON.stringify(obj) === JSON.stringify(newValue) ? obj : newValue;
+      map.set(selectorFunc!, finalValue);
+      return finalValue as U;
     };
     const getSnapshot = () => (selectorFunc ? getSlice() : stateRef.k) as U;
     return useSyncExternalStore(
